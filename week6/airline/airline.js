b4b298d0b53a48f3d1a0f120bae48f5d4d3582dc
BrenSeibel <brens.seibel@gmail.com>
2020-11-02 00:40:38 -0500

    not ready

---
+++ week6/airline/airline.js
@@ -0,0 +1,21 @@
+
+var x = document.seibel_airline;
+
+x.addEventListener("submit", function(event){
+    event.preventDefault()
+    var rainbow = []
+    for (i = 0; i < x.diet.length; i++){
+        console.log(x.diet)
+        if (x.diet[i].checked){
+            rainbow.push(x.diet[i].value)
+        }
+    }
+    alert(`First Name: ${x.fname.value}
+    Last Name: ${x.lname.value}
+    Age: ${x.Age.value}
+    Gender: ${x.gender.value}
+    Destination: ${x.country.value}
+    Diet: ${rainbow}`)
+    })
+
+
